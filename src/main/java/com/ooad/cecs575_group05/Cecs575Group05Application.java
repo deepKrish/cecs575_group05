6517a59bb96160f6070172bebd32359505d7b95d
Deepa Padhee <deepadhee@Deepas-MBP.lan>
2022-03-14 15:23:30 -0700

    New Repo Commit

---
+++ src/main/java/com/ooad/cecs575_group05/Cecs575Group05Application.java
@@ -0,0 +1,13 @@
+package com.ooad.cecs575_group05;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cecs575Group05Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cecs575Group05Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/ooad/cecs575_group05/Cecs575Group05ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ooad.cecs575_group05;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cecs575Group05ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
